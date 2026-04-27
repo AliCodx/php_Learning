46fe1e719a87bcc04f770af6fad9c490a498843b
Ali Abid <ali.abid3889@gmail.com>
2026-04-27 14:55:08 +0500

    File Creating in Php

---
+++ fileCreatingInPhp/logic.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST["create&Save"])){
+  
+    if(isset($_POST["fileName"])){
+    $fileName = $_POST["fileName"];
+}
+    $file = fopen($fileName,"w");
+
+    if(isset($_POST["content"])){
+    $content = $_POST["content"];
+}
+    if(fwrite($file,$content)){
+        echo "File is Created and content Saved";
+    }
+    fclose($file);
+}
+
+
